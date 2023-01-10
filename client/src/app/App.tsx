9f18ac2744cfb306638969c2a8a74f245a7e5038
DanielOhn <ohndaniel@gmail.com>
2023-01-10 16:15:36 -0700

    Update CSS for main app.  Update gitignore.

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,18 +1,21 @@
 import './App.css'
 
-import InputPost from "../posts/InputPost"
-import ListPosts from "../posts/ListPosts"
+import InputPost from '../posts/InputPost'
+import ListPosts from '../posts/ListPosts'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Posts</h1>
-      <p>Creating posts</p>
+    return (
+        <div className="App">
+            <h1>Posts</h1>
+            <p>Creating posts</p>
 
-      <InputPost />
-      <ListPosts />
-    </div>
-  )
+            {/* <InputPost /> */}
+            {/* <ListPosts /> */}
+            <footer>
+                <div>Footer Goes Here</div>
+            </footer>
+        </div>
+    )
 }
 
 export default App
