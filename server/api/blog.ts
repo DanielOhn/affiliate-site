48d4dd17e0a83ab11285b3a2ae775af2742c268f
DanielOhn <ohndaniel@gmail.com>
2023-02-07 00:13:09 -0700

    Add Express Routes to the blog api.  Use express routes for index to connect to api middleware.

--- server/api/blog.ts
+++ server/api/blog.ts
@@ -1,57 +1,83 @@
 import pool from '../database'
-import app from '../index'
+import * as express from 'express'
+
+const router = express.Router()
 
 // POST blog
-app.post(`/blog`, async (req: any, res: any) => {
-    try {
-        const { title, content, author } = req.body
+router
+    .route('/blog')
+    .post(async (req: any, res: any) => {
+        try {
+            const { title, content } = req.body
+            const author = 1
 
-        const created = `POSTGRES GET DATE NOW()` // FIX THIS
+            const time = await pool.query(`SELECT NOW()`)
+            const created = time.rows[0].now
 
-        const newBlog = await pool.query(
-            `INSERT INTO blog (title, content, author, created) VALUES ($1, $2, $3, $4) RETURNING *`,
-            [title, content, author, created]
-        )
-    } catch (err) {
-        console.error(err)
-    }
-})
+            console.log(created)
+
+            const newBlog = await pool.query(
+                `INSERT INTO blog (title, content, author, created) VALUES ($1, $2, $3, $4) RETURNING *`,
+                [title, content, author, created]
+            )
+
+            res.json(newBlog.rows[0])
+        } catch (err) {
+            console.error(err)
+        }
+    })
+    // DELETE
+    .delete(async (req: any, res: any) => {
+        try {
+            const { blog_id } = req.params
+
+            const deleteBlog = await pool.query(
+                'DELETE FROM blog WHERE blog_id = $1',
+                [blog_id]
+            )
+
+            res.json(`Blog ${blog_id} was deleted`)
+        } catch (err) {
+            console.error(err)
+        }
+    })
 
 // UPDATE blog
-app.put(`/blog/:blog_id`, async (req: any, res: any) => {
-    try {
-        const { updateTitle, updateContent } = req.body
-        const { blog_id } = req.params
+router
+    .route(`/blog/:blog_id`)
+    .put(async (req: any, res: any) => {
+        try {
+            const { updateTitle, updateContent } = req.body
+            const { blog_id } = req.params
 
-        const updateBlog = await pool.query(
-            `UPDATE blog SET title = $1, content = $2 WHERE blog_id = $3`,
-            [updateTitle, updateContent, blog_id]
-        )
+            const updateBlog = await pool.query(
+                `UPDATE blog SET title = $1, content = $2 WHERE blog_id = $3`,
+                [updateTitle, updateContent, blog_id]
+            )
 
-        res.json(updateBlog.rows[0])
-    } catch (err) {
-        console.error(err)
-    }
-})
+            res.json(updateBlog.rows[0])
+        } catch (err) {
+            console.error(err)
+        }
+    })
+    // GET blog
+    .get(async (req: any, res: any) => {
+        try {
+            const { blog_id } = req.params
 
-// GET blog
-app.get(`/blog/:blog_id`, async (req: any, res: any) => {
-    try {
-        const { blog_id } = req.params
+            const allBlogs = await pool.query(
+                'SELECT * FROM blog WHERE blog_id = $1',
+                [blog_id]
+            )
 
-        const allBlogs = await pool.query(
-            'SELECT * FROM blog WHERE blog_id = $1',
-            [blog_id]
-        )
-
-        res.json(allBlogs.rows[0])
-    } catch (err) {
-        console.error(err)
-    }
-})
+            res.json(allBlogs.rows[0])
+        } catch (err) {
+            console.error(err)
+        }
+    })
 
 // GET blogs
-app.get(`/blogs`, async (req: any, res: any) => {
+router.route('/blogs').get(async (req: any, res: any) => {
     try {
         const allBlogs = await pool.query(`SELECT * FROM blog`)
 
@@ -61,4 +87,4 @@ app.get(`/blogs`, async (req: any, res: any) => {
     }
 })
 
-// DELETE
+module.exports = router
--- server/index.ts
+++ server/index.ts
@@ -4,66 +4,11 @@ import pool from './database'
 const app = express()
 const port = 8000
 
+const blog = require('./api/blog')
+
 app.use(express.json())
-
-// GET posts
-app.get('/posts', async (req: any, res: any) => {
-    try {
-        const allPosts = await pool.query('SELECT * FROM posts')
-        res.json(allPosts.rows)
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-// CREATE posts
-app.post('/posts', async (req: any, res: any) => {
-    try {
-        const { text } = req.body
-        const newPost = await pool.query(
-            'INSERT INTO posts (text) VALUES ($1) RETURNING *',
-            [text]
-        )
-
-        res.json(newPost.rows[0])
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-// EDIT posts
-app.put('/posts/:post_id', async (req: any, res: any) => {
-    try {
-        const { post_id } = req.params
-        const { updateText } = req.body
-
-        const updatePost = await pool.query(
-            'UPDATE posts SET text = $1 WHERE post_id = $2',
-            [updateText, post_id]
-        )
-        res.json('Post was updated.')
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-// DELETE posts
-app.delete('/posts/:post_id', async (req: any, res: any) => {
-    try {
-        const { post_id } = req.params
-        const deletePost = await pool.query(
-            'DELETE FROM posts WHERE post_id = $1',
-            [post_id]
-        )
-
-        res.json('Post was deleted.')
-    } catch (err) {
-        console.error(err)
-    }
-})
+app.use('/api', blog)
 
 app.listen(port, () => {
     console.log(`Running server on http://localhost:${port}`)
 })
-
-export default app
