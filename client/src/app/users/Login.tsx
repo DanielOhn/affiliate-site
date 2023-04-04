4f98995a63de2aeef3976110ad1d82911e66c751
DanielOhn <ohndaniel@gmail.com>
2023-04-03 23:42:57 -0700

    Update css for login fail.  Update Login and Logout with navigation.

--- client/src/app/users/Login.tsx
+++ client/src/app/users/Login.tsx
@@ -6,6 +6,8 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
 
+    const [loginFail, setLoginFail] = useState(false)
+
     const onSubmit = async (e: SyntheticEvent) => {
         e.preventDefault()
 
@@ -17,10 +19,18 @@ const Login = () => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(body),
             })
-            navigate('/')
-            navigate(0)
-        } catch (err) {
-            console.error(err)
+
+            const data: boolean = await login.json()
+
+            if (data) {
+                navigate('/')
+                navigate(0)
+            } else {
+                setLoginFail(true)
+            }
+        } catch (err: any) {
+            console.log(err)
+            setLoginFail(true)
         }
     }
 
@@ -43,6 +53,13 @@ const Login = () => {
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 <br />
+                {loginFail ? (
+                    <div className="failed">
+                        * failed to login, please try again
+                    </div>
+                ) : (
+                    <></>
+                )}
                 <button>Login</button>
             </form>
         </>
--- client/src/app/users/Logout.tsx
+++ client/src/app/users/Logout.tsx
@@ -4,23 +4,23 @@ import { useNavigate } from 'react-router-dom'
 const Logout = () => {
     const navigate = useNavigate()
 
-    const Logout = async (e: SyntheticEvent) => {
+    const Signout = async (e: SyntheticEvent) => {
         e.preventDefault()
 
         try {
             const logout = await fetch('/api/logout')
 
-            navigate(0)
             navigate('/')
+            navigate(0)
         } catch (err) {
             console.log(err)
         }
     }
 
     return (
-        <>
-            <button onClick={(e) => Logout(e)}>Logout</button>
-        </>
+        <div>
+            <button onClick={(e) => Signout(e)}>Logout</button>
+        </div>
     )
 }
 
